2f5684dc669c52e82a107162fb06d35bd3a96e71
spython <emmanuelegbo01@gmail.com>
2023-09-08 23:25:58 +0100

    modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,28 @@
 import Home from "./pages/home/Home";
-//import Profile from "./pages/profile/Profile";
-//import Login from "./pages/login/Login";
-//import Register from "./pages/register/Register";
+import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/profile/:username">
+          <Profile />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export 
