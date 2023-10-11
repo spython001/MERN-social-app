ae97acc3beb46b1d489351b5e035b6a907525895
spython <emmanuelegbo01@gmail.com>
2023-10-11 15:29:02 +0100

    modified files

--- client/src/pages/profile/Profile.jsx
+++ client/src/pages/profile/Profile.jsx
@@ -5,22 +5,25 @@ import Rightbar from '../../components/rightbar/Rightbar'
 import "./profile.css"
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 export default function Profile() {
-  const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER
-  const [user, setUser] = useState({})
+  const PF = import.meta.env.VITE_REACT_APP_PUBLIC_FOLDER;
+  const [user, setUser] = useState({});
+  const username = useParams().username;
+  
 
   useEffect(() => {
     const fetchUser = async () => { 
       try {
-        const res = await axios.get(`http://localhost:8800/api/users?username=john`);
+        const res = await axios.get(`http://localhost:8800/api/users?username=${username}`);
         setUser(res.data);
       } catch (error) {
         console.error("Error fetching user:", error);
       }
     };
     fetchUser();
-  }, []);
+  }, [username]);
 
   return (
     <>
@@ -30,8 +33,8 @@ export default function Profile() {
         <div className="profileRight">
             <div className="profileRightTop">
                 <div className="profileCover">
-                    <img className='profileCoverImg' src= {`${PF}person/3.jpeg`} alt="" />
-                    <img className="profileUserImg" src={`${PF}person/7.jpeg`} alt="" />
+                    <img className='profileCoverImg' src= {user.coverPicture || PF+"person/noCover (2).png"} alt="" />
+                    <img className="profileUserImg" src= {user.coverPicture || PF+"person/noAvatar.png"} alt="" />
                 </div>
                 <div className="profileInfo">
                     <h4 className='profileInfoName'>{user.username}</h4>
@@ -39,7 +42,7 @@ export default function Profile() {
                 </div>
             </div>
             <div className="profileRightBottom">
-                <Feed username="john"/> 
+                <Feed username={username}/> 
                 <Rightbar user={user}/>
             </div>      
         </div>    
