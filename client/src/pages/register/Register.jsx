f1ce04f8dd70899c6dea9ff205f5598a03cad553
spython <emmanuelegbo01@gmail.com>
2023-10-13 07:34:10 +0100

    modified files

--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,6 +1,36 @@
 import "./register.css"
+import { useRef } from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
 
 export default function Register() {
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const confirmPassword = useRef();
+  const history = useHistory()
+
+  const handleClick = async (e) =>{
+    e.preventDefault();
+    if(confirmPassword.current.value !== password.current.value){
+        confirmPassword.current.setCustomValidity("Passwords don't match!");
+    }else{
+        const user = {
+            username: username.current.value,
+            email: password.current.value,
+            password: password.current.value,
+        }
+        try {
+            await axios.post("http://localhost:8800/api/auth/register", user);
+            history.push("/login");
+        } catch (err) {
+            console.log(err)
+        }
+    }
+  };
+
+
+    
   return (
     <div className="login">
         <div className="loginWrapper">
@@ -9,16 +39,33 @@ export default function Register() {
                 <span className="loginDesc">A different vibe and connection on Spython-Social.</span>
             </div>
             <div className="loginright">
-                <div className="loginBox">
-                    <input placeholder="Username" className="loginInput" />
-                    <input placeholder="Email" className="loginInput" />
-                    <input  placeholder="Password" className="loginInput" />
-                    <input placeholder="Confirm Password" className="loginInput" />
-                    <button className="loginButton">Sign Up</button>
+                <form className="loginBox" onSubmit={handleClick}>
+                    <input placeholder="Username" required ref={username} className="loginInput" />
+                    <input 
+                      placeholder="Email" 
+                      required ref={email} 
+                      className="loginInput" 
+                      type="email"
+                    />
+                    <input 
+                      placeholder="Password" 
+                      required ref={password} 
+                      className="loginInput" 
+                      type="password"
+                      minLength="6"
+                    />
+                    <input 
+                      placeholder="Confirm Password" 
+                      required ref={confirmPassword} 
+                      className="loginInput" 
+                      type="password"
+                    />
+                    <button className="loginButton" type="submit">Sign Up</button>
                     <button className="loginRegisterButton">Log into Account</button>
-                </div>
+                </form>
             </div>
         </div>
     </div>
   )
 }
+ 
